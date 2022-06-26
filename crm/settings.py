4452174737f2f6500897f25013b7524b8eb3b93a
zebra-f <rafal.zebrowski@gmail.com>
2022-06-26 21:28:27 +0200

    static setup

--- crm/settings.py
+++ crm/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATIC_ROOT = 'static_root'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- crm/urls.py
+++ crm/urls.py
@@ -15,11 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('landing.urls', namespace='landing')),
-    path('leads/', include('leads.urls', namespace='leads'))
+    path('leads/', include('leads.urls', namespace='leads')),
+
+    # static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
