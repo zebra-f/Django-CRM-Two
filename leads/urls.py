87a39d39b1418c65401e66d91c66399f6e9d2056
zebra-f <rafal.zebrowski@gmail.com>
2022-06-28 23:41:44 +0200

    changed function views to class based views

--- leads/urls.py
+++ leads/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 from .views import (
-    lead_list,
-    lead_detail, 
-    lead_create, 
-    lead_update, 
+    LeadListView,
+    LeadDetailView, 
+    LeadCreateView, 
+    LeadUpdateView, 
     lead_delete,
     )
 
@@ -13,9 +13,9 @@ from .views import (
 app_name = 'leads'
 
 urlpatterns = [
-    path('', lead_list, name='lead-list'),
-    path('<int:pk>/', lead_detail, name='lead-detail'),
-    path('<int:pk>/update/', lead_update, name='lead-update'),
+    path('', LeadListView.as_view(), name='lead-list'),
+    path('<int:pk>/', LeadDetailView.as_view(), name='lead-detail'),
+    path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
     path('<int:pk>/delete/', lead_delete, name='lead-delete'),
-    path('create/', lead_create, name='lead-create'),
+    path('create/', LeadCreateView.as_view(), name='lead-create'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,77 +1,83 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect  
+from django.urls import reverse, reverse_lazy
 from django.http import HttpResponse
+from django.views.generic import (
+    TemplateView, ListView, DeleteView, DetailView, RedirectView, CreateView, UpdateView
+    )
 
 from .models import Lead, Agent, User
 from .forms import LeadModelForm
 
 # Create your views here.
 
-def lead_list(request):
+class LeadListView(ListView):
 
-    leads = Lead.objects.all()
+    template_name: str = 'leads/lead_list.html'
+    model = Lead
+    context_object_name = 'leads'
     
-    context = {
-        "name": "Joe",
-        "age": 45,
-        "leads": leads
-    }
 
-    return render(request, 'leads/lead_list.html', context)
+class LeadDetailView(DetailView):
 
-
-def lead_detail(request, pk):
-
-    lead = Lead.objects.get(id=pk)
-
-    context = {
-        "lead": lead
-    }
-
-    return render(request, 'leads/lead_detail.html', context)
+    template_name = 'leads/lead_detail.html'
+    model = Lead
+    context_object_name = 'lead'
 
 
-def lead_create(request):
+class LeadCreateView(CreateView):
 
-    if request.method == 'POST':
-        # ModelForm
-        form = LeadModelForm(request.POST)
+    template_name = 'leads/lead_create.html'
+    form_class = LeadModelForm
+    success_url = reverse_lazy('leads:lead-list')
+
+# def lead_create(request):
+
+#     if request.method == 'POST':
+#         # ModelForm
+#         form = LeadModelForm(request.POST)
         
-        if form.is_valid():
-            # form.cleaned_data returns a dictionary
-            # Lead.objects.create(**form.cleaned_data)
-            form.save()       
+#         if form.is_valid():
+#             # form.cleaned_data returns a dictionary
+#             # Lead.objects.create(**form.cleaned_data)
+#             form.save()       
 
-            return redirect('/leads')
+#             return redirect('/leads')
 
-    form = LeadModelForm()
+#     form = LeadModelForm()
 
-    context = {
-        "form": form
-    }
+#     context = {
+#         "form": form
+#     }
 
-    return render(request, 'leads/lead_create.html', context)
+#     return render(request, 'leads/lead_create.html', context)
+
+class LeadUpdateView(UpdateView):
+    template_name = 'leads/lead_update.html'
+    model = Lead
+    form_class = LeadModelForm
+    success_url = reverse_lazy('leads:lead-list')
 
 
-def lead_update(request, pk):
+# def lead_update(request, pk):
      
-    lead = Lead.objects.get(id=pk)
+#     lead = Lead.objects.get(id=pk)
 
-    if request.method == 'POST':
-        form = LeadModelForm(request.POST, instance=lead)
+#     if request.method == 'POST':
+#         form = LeadModelForm(request.POST)
         
-        if form.is_valid():
-            form.save()
+#         if form.is_valid():
+#             form.save()
 
-            return redirect('/leads')
+#             return redirect('/leads')
 
-    form = LeadModelForm(instance=lead)
+#     form = LeadModelForm(instance=lead)
     
-    context = {
-        "lead": lead,
-        "form": form
-    }
+#     context = {
+#         "lead": lead,
+#         "form": form
+#     }
 
-    return render(request, 'leads/lead_update.html', context)
+#     return render(request, 'leads/lead_update.html', context)
 
 
 def lead_delete(request, pk):
