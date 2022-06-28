d7c8e7f6365cef0e67533d6d4a9e08dd60e98bbd
zebra-f <rafal.zebrowski@gmail.com>
2022-06-28 23:55:22 +0200

    removed greyed functions

--- leads/views.py
+++ leads/views.py
@@ -30,54 +30,13 @@ class LeadCreateView(CreateView):
     form_class = LeadModelForm
     success_url = reverse_lazy('leads:lead-list')
 
-# def lead_create(request):
-
-#     if request.method == 'POST':
-#         # ModelForm
-#         form = LeadModelForm(request.POST)
-        
-#         if form.is_valid():
-#             # form.cleaned_data returns a dictionary
-#             # Lead.objects.create(**form.cleaned_data)
-#             form.save()       
-
-#             return redirect('/leads')
-
-#     form = LeadModelForm()
-
-#     context = {
-#         "form": form
-#     }
-
-#     return render(request, 'leads/lead_create.html', context)
 
 class LeadUpdateView(UpdateView):
+    
     template_name = 'leads/lead_update.html'
     model = Lead
     form_class = LeadModelForm
     success_url = reverse_lazy('leads:lead-list')
-
-
-# def lead_update(request, pk):
-     
-#     lead = Lead.objects.get(id=pk)
-
-#     if request.method == 'POST':
-#         form = LeadModelForm(request.POST)
-        
-#         if form.is_valid():
-#             form.save()
-
-#             return redirect('/leads')
-
-#     form = LeadModelForm(instance=lead)
-    
-#     context = {
-#         "lead": lead,
-#         "form": form
-#     }
-
-#     return render(request, 'leads/lead_update.html', context)
 
 
 def lead_delete(request, pk):
