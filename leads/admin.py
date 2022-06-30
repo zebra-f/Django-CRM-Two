7063d9764176e8cc040ce9e8b11b2cd4a32838ee
zebra-f <rafal.zebrowski@gmail.com>
2022-06-30 16:20:17 +0200

    Affiliation model, singals

--- leads/admin.py
+++ leads/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import User, Lead, Agent
+from .models import User, Lead, Agent, Affiliation
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Lead)
 admin.site.register(Agent)
+admin.site.register(Affiliation)
--- leads/models.py
+++ leads/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 
 # Create your models here.
 
 class User(AbstractUser):
     pass 
+
+
+class Affiliation(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    user = models.OneToOneField("User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        if self.name:
+            return f"{self.user.username}, {self.name}"
+        else:
+            return self.user.username
 
 
 class Lead(models.Model):
@@ -29,12 +41,27 @@ class Lead(models.Model):
     agent = models.ForeignKey("Agent", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return f"({self.id})- {self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name}"
 
 
 class Agent(models.Model):
 
     user = models.OneToOneField("User", on_delete=models.CASCADE)
+    affiliation = models.ForeignKey("Affiliation", on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"({self.id})- {self.user.email}"
+        return f"{self.user.email}"
+
+
+# Signals 
+
+def post_user_created_singal(sender, instance, created, **kwargs):
+    
+    # print(instance, created, sender)
+    if created:
+        Affiliation.objects.create(user=instance)
+    else:
+        pass
+    
+
+post_save.connect(post_user_created_singal, sender=User)
