6232e68f0c9af9a3c57eb2685130061a9ee553d4
zebra-f <rafal.zebrowski@gmail.com>
2022-07-04 23:54:04 +0200

    notification delete button, js eventlistener

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+document.addEventListener("DOMContentLoaded", () => {
+  (document.querySelectorAll(".notification .delete") || []).forEach(
+    ($delete) => {
+      const $notification = $delete.parentNode;
+
+      $delete.addEventListener("click", () => {
+        $notification.parentNode.removeChild($notification);
+      });
+    }
+  );
+});
